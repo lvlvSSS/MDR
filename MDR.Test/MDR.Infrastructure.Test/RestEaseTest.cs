67c80b947207fda002c1f6bcb6f1717389b7dca8
lvlvSSS <nelson.lv.007@outlook.com>
2024-02-19 14:08:35 +0800

    add unit test for RestEase

---
+++ MDR.Test/MDR.Infrastructure.Test/RestEaseTest.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Threading.Tasks;
+using MDR.Infrastructure.Extensions;
+using MDR.Infrastructure.Log.Implementation;
+using MDR.Infrastructure.RestEase;
+using MDR.Infrastructure.RestEase.Common;
+using Newtonsoft.Json;
+using NLog.Config;
+
+namespace MDR.Infrastructure.Test
+{
+    public class RestEaseTest
+    {
+        [Fact]
+        public void TestRestEase()
+        {
+            var client = new RestClient("http://localhost:5000/")
+            {
+                JsonSerializerSettings = new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore, DefaultValueHandling = DefaultValueHandling.Ignore, Formatting = Formatting.Indented }
+            }.For<IWeatherForecast>();
+            var result = client.GetWeatherForecasts().Result;
+            new NLog4Logging().Info(result);
+        }
+    }
+
+    public interface IWeatherForecast
+    {
+        [Get("weatherforecast/Get")]
+        Task<List<WeatherForecast>> GetWeatherForecasts();
+    }
+
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF { get; set; }
+
+        public string? Summary { get; set; }
+    }
+
+}
