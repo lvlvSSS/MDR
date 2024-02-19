336460d77c01c1c1cfe64ccab0592ce2c426c8df
lvlvSSS <nelson.lv.007@outlook.com>
2024-02-19 14:24:59 +0800

    do some modification in build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ echo "build solution ..."
 dotnet build MDR.sln -c Release
 echo "build solution end!"
 
+# deal with MDR.Infrastructure.Log
+echo "ready to deal with MDR.Infrastructure.Log ..."
 echo "copy the NLog.config to release folder ..."
 cp ./MDR.Infrastructure/MDR.Infrastructure.Log/NLog.config ./MDR.Server/bin/Release/net8.0
 echo "copy the NLog.config end!"
+echo "MDR.Infrastructure.Log is done!"
